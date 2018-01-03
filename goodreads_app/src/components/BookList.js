c56b0975b9ba55bd02bfae02671d89f6ce5c40b7
Steven Li <steven4354@gmail.com>
2018-01-03 14:43:28 -0800

    new push

--- goodreads_app/src/components/BookList.js
+++ goodreads_app/src/components/BookList.js
@@ -6,7 +6,10 @@ const BookList = ({books}) => {
     <div className="row">
       {books.map(book => {
         return (
-          <div className="col-xs-4 card">
+          <div
+            key={book.best_book[0].small_image_url[0]}
+            className="col-xs-4 card"
+          >
             <img src={book.best_book[0].small_image_url[0]} />
             <p> Title: {book.best_book[0].title[0]} </p>
             <p> Author: {book.best_book[0].author[0].name[0]} </p>
